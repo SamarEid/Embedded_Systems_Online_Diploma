b8b43c5047cfe795fdb2a1aa7b92df3365d02c80
Samar Gamal <samargamalmahmoud@gmail.com>
2023-10-01 20:00:02 +0300

    Arrays Assignment

---
+++ Unit2/Lesson4/Arrays/Ex2.c
@@ -0,0 +1,18 @@
+//
+// Created by dell on 10/1/2023.
+//
+#include "stdio.h"
+int main(){
+    int n;
+    printf("Enter the number of data: ");
+    scanf("%d", &n);
+    float sum = 0.0;
+    for (int i = 1; i <= n; i++){
+        float x;
+        printf("%d. Enter number: ", i);
+        scanf("%f", &x);
+        sum += x;
+    }
+    float avg = sum / n;
+    printf("Average = %.2f", avg);
+}
